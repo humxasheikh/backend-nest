64f099174206ab79f04ae4e7b060826d26b248a2
Hamza Arshad <hamza.arshad1008@gmail.com>
2025-07-18 15:07:26 +0200

    added the implementation to generate the auth token

--- task-app-01/src/auth/auth.service.ts
+++ task-app-01/src/auth/auth.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { retry } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<{ email: string; _id: string } | null> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(email: string, password: string) {
+    const user = await this.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return {
+      access_token: this.jwtService.sign({ email: user.email, sub: user._id }),
+    };
   }
 }
--- task-app-01/src/users/users.service.ts
+++ task-app-01/src/users/users.service.ts
@@ -19,7 +19,7 @@ export class UsersService {
     return createUser.save();
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string): Promise<(User & { _id: string }) | null> {
     return this.userModel.findOne({ email });
   }
 }
